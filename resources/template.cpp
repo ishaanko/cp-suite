d51fff31be5d0ffccf7f3fb26d134639fdabe8cb
Ishaan Kothari <imishaanko@gmail.com>
2024-01-02 21:56:53 -0800

    Useful stuff

---
+++ resources/template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+using ll = long long;
